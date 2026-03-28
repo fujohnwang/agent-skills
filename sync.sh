ed5f299ac3c535e757a1c4b48950773900772ece
af <fq@keevol.cn>
2026-03-28 16:46:21 +0800

    update sync script

--- sync.sh
+++ sync.sh
@@ -1,1 +1,3 @@
-cp -as "$(pwd)/"* "$HOME/.claude/skills/"
+# cp -as "$(pwd)/"* "$HOME/.claude/skills/"
+# exclude other files and only copy subfolders as link 
+find . -mindepth 1 -type d -exec cp -as "{}" "$HOME/.claude/skills/{}" \;
